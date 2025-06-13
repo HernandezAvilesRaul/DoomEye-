3b7f99b5499f484cbb118019f40e168bf04694a2
HernandezAvilesRaul <20223tn056@utez.edu.mx>
2025-06-12 21:50:09 -0600

    Pruebas enviar entornos

--- Model/esp32Config/esp32Test/src/main.cpp
+++ Model/esp32Config/esp32Test/src/main.cpp
@@ -1,16 +1,41 @@
-#include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 
-const int ledPin = 13;  
+const char* ssid = "Totalplay-E64E";
+const char* password = "196AE64E";
+const char* serverUrl = "http://192.168.100.41:5000/estado_boton";
+
+const int botonPin = 13; 
+bool estado = false;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(botonPin, INPUT_PULLUP); 
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando al WiFi...");
+  }
+  Serial.println("Conectado al WiFi.");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);  
-  
-  digitalWrite(ledPin, LOW);
-  delay(1000);  
+  estado = !estado;
+  String valor = (estado == HIGH) ? "HIGH" : "LOW";
+
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    String json = "{\"estado\":\"" + valor + "\"}";
+    int code = http.POST(json);
+    Serial.println("Estado del botón enviado: " + valor);
+    Serial.println("Código de respuesta: " + String(code));
+    http.end();
+  }
+
+  delay(3000);
 }
